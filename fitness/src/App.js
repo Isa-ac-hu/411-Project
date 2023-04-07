933ef74a91b5a718ab1243027ffe761e0bb531f7
rchung14 <ryanchung1430@gmail.com>
2023-04-07 04:12:01 -0400

    minor bug fixes

--- fitness/src/App.js
+++ fitness/src/App.js
@@ -4,6 +4,8 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Protected from './components/Protected';
 import { AuthContextProvider } from './context/AuthContext';
+import Account from './templates/Account';
+import Home from './templates/Home';
 import Signin from './templates/Login';
 
 function App() {
@@ -20,7 +22,16 @@ function App() {
         <AuthContextProvider>
         <Navbar />
         <Routes>
+          <Route path='/' element={<Home />} />
           <Route path='/signin' element={<Signin />} />
+          <Route
+            path='/account'
+            element={
+              <Protected>
+                <Account />
+              </Protected>
+            }
+          />
         </Routes>
         </AuthContextProvider>
         </div>
