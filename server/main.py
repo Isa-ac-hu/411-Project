d4e1885f58ee8c1aee49589e49a6f6947c84ecf8
rchung14 <ryanchung1430@gmail.com>
2023-04-26 12:38:02 -0400

    database has unique user_id for every request

--- server/main.py
+++ server/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 from firebase_admin import credentials, firestore, initialize_app
-from flask import Flask, jsonify, request
 
 app = Flask(__name__, static_folder="../fitness/public", static_url_path="/")
 cred = credentials.Certificate("./serviceAccountKey.json")
@@ -15,28 +14,29 @@ def index():
 @app.route('/food', methods=['POST'])
 def add_food():
     data = request.get_json()
-    # Prints the data that was received in console 
-    print(f"Received data: {data}")
-    # user_id = data.get('user_id') --> not sure about user_id but some kind of primary key in database 
+    uid = data.get('user_id')
     food_items = data.get('food_items')
+    print(data)
     
     # Extract relevant nutrition info
-    nutrition_info = {
+    food_info = {
+        'user_id': uid,
+        'name': food_items[0]['name'],
         'calories': food_items[0]['nutrition_info']['calories'],
         'protein': food_items[0]['nutrition_info']['protein'],
         'fat': food_items[0]['nutrition_info']['fat'],
-        'carbohydrates': food_items[0]['nutrition_info']['carbohydrates']
+        'carbohydrates': food_items[0]['nutrition_info']['carbohydrates'],
+        'quantity': food_items[0]['quantity']
     }
 
     # Inserting into firebase 
-    # This should be database https://console.cloud.google.com/firestore/databases/-default-/data/panel/foods/8DaVsmgd6cZ6RArADRl2?project=project-9effe
     doc_ref = db.collection('foods').document()
     doc_ref.set({
-        'food_items': food_items,
-        'nutrition_info': nutrition_info
+        'food_info': food_info
     })
 
-    return jsonify({'success': True},)
+    return jsonify({'success': True})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
