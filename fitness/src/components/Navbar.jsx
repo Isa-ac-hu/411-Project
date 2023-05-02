d02531ad2519be38e85db874b630ce92504fc5e8
Isaac Hu <isaac.hu002@gmail.com>
2023-05-02 13:40:47 -0400

    added CSS

--- fitness/src/components/Navbar.jsx
+++ fitness/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
+import '../App.css';
 
 const Navbar = () => {
   const { user, logOut } = UserAuth();
@@ -14,26 +15,45 @@ const Navbar = () => {
   }
 
   return (
-    <div style={{ backgroundColor: '#f1f1f1' }} className='w-full'>
+
+      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '20px', backgroundColor: '#eee', width: '100%'}}>
       <div className='flex justify-between p-4'>
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          <h1 style={{ fontSize: '48px', margin: '0', fontWeight: 'bold' }}>YourFitnessPRO</h1>
+          <h1 style={{ fontSize: '48px', margin: '0', fontWeight: 'bold',  fontFamily: 'Inter' }}>YourFitnessPRO</h1>
         </div>
         <div>
           <br />
-          <Link style={{ marginLeft: '20' }} to='/'>Home</Link>
-          {user?.displayName ? (
-            <>
-              <Link style={{ marginLeft: '10px' }} to='/account'>Profile</Link>
-              <Link style={{ marginLeft: '10px' }} to='/signin' onClick={handleSignOut}>Sign out</Link>
-            </>
-          ) : (
-            <Link style={{ marginLeft: '10px' }} to='/signin'>Sign in</Link>
-          )}
+
+            <nav>
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    {user?.displayName ? (
+                        <>
+                            <li><Link to='/account'>Profile</Link></li>
+                            <li><Link to='/signin' onClick={handleSignOut}>Sign out</Link></li>
+                        </>
+                    ) : (
+                        <li><Link to='/signin'>Sign in</Link></li>
+                    )}
+                    <li className="contact"><Link to='/contact'>Contact</Link></li>
+                </ul>
+            </nav>
+
+
         </div>
       </div>
     </div>
   );
 };
-
+// function TopNav() {
+//   return (
+//       <nav>
+//         <a href="#home" className="active">Home</a>
+//         <a href="#about">About</a>
+//         <a href="#contact">Contact</a>
+//         <a href="#news">News</a>
+//         <a href="#blog">Blog</a>
+//       </nav>
+//   );
+// }
 export default Navbar;
